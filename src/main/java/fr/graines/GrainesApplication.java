526e2a211c0db2f1d9d287197a03829fa44fb06e
ChineDmitri <mr.dima11@gmail.com>
2024-04-03 08:32:46 +0200

    Init repository

---
+++ src/main/java/fr/graines/GrainesApplication.java
@@ -0,0 +1,13 @@
+package fr.graines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrainesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrainesApplication.class, args);
+	}
+
+}
